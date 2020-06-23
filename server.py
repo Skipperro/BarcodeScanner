1e13d0c6c08ec5676e87e883da04f84dd64089e4
Tomasz <skipperro@gmail.com>
2020-06-23 19:52:49 +0200

    Release version

--- server.py
+++ server.py
@@ -63,6 +63,6 @@ def process_post():
 
 
 if __name__ == '__main__':
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=5000)
-    app.run(host='0.0.0.0', debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
+    #app.run(host='0.0.0.0', debug=True)
