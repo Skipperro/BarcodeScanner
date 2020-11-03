e34dbf8485b32e741c0ea83e87f09d652e884664
Tomasz Dylewski <t.dylewski@mifcom.de>
2020-11-03 14:33:58 +0100

    Change port to 80

--- server.py
+++ server.py
@@ -64,5 +64,5 @@ def process_post():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=80)
     #app.run(host='0.0.0.0', debug=True)
